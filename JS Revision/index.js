59bef8ea43abeb29254006ace6a8d399a318ab34
gaurav4883 <gaurav.deol1600@gmail.com>
2023-03-02 00:59:43 +0545

    js refreshing

---
+++ JS Revision/index.js
@@ -0,0 +1,36 @@
+// let a = 34
+// let a = "hello"
+let a = 45
+a = 78
+console.log(a);
+
+/*Data Types: Number, Objects, Booleans, String, undefined, null */
+
+let hello = () => {
+    console.log("Hello code 123!");
+}
+
+function hello2() {
+    console.log("234");
+}
+
+hello()
+hello2()
+
+let obj1 = {
+    r: 34,
+    t: 45,
+    g: 56
+}
+console.log(obj1, obj1.t);
+document.addEventListener('click', () => {
+    confirm("are you sure? you want to delete File?")
+    alert("Deleted File !")
+})
+
+arr1 = [2, 3, 4, 5,]
+arr1.forEach(element => {
+    console.log(element);
+});
+
+console.log(this);
